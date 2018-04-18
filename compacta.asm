476d4813428cb83375ccccdf835b7e9766b9aa07
Pedro Saman <pedrosaman96@gmail.com>
2018-04-17 23:14:39 -0300

    Added the first compaction prototype
    In here I developed the initial idea for compaction. Among them, how to open the files for read and write, how to read one char by one and concatenate then in a single buffer. After that how to write the concatenated buffer in a new file.

---
+++ compacta.asm
@@ -0,0 +1,68 @@
+.data
+	
+	arquivotexto: .asciiz "arquivotexto.txt"# Nome do arquivo com o texto a ser compactado
+	dicionario: .asciiz "dicionario.txt"	# Nome do arquivo com o dicionário
+	buffer_auxiliar: .asciiz ""		# Buffer para a leitura individual de cada caractere do arquivo a ser compactado
+	buffer_de_comparação: .asciiz ""	# Buffer que vai concatenando os caracteres lidos e usado para comparar com os valores do dicionario
+	
+.text
+	
+Abre_Arquivos: # Abre os arquivos textos que seram necessários no programa
+	
+	li $v0, 13		# Código  do open file
+	la $a0, dicionario	# Label do arquivo a ser lido
+	li $a1, 9		# flag 9
+	li $a2, 0		# Modo de escrita e append
+	syscall			# Abre o arquivo texto do dicionario 
+	move $s1, $v0		# Guarda o descritor do arquivo
+	
+	li $v0, 13		# Código  do open file
+	la $a0, arquivotexto	# Label do arquivo a ser lido
+	li $a1, 0		# flag 0
+	li $a2, 0		# Modo de leitura
+	syscall			# Abre o arquivo texto a ser compactado
+	move $s0, $v0		# Guarda o descritor do arquivo
+	
+	la $t1, buffer_de_comparação($zero) # Põe em $t1 o endereço do buffer_de_comparação, serve para concatenar os chars nele
+	
+Pega_proximo: # Põe em buffer_auxiliar o proximo caractere a ser lido do arquivo original
+	
+	li $v0, 14		# Código do read file
+	move $a0, $s0		# Descritor do arquivo a ser lido
+	la $a1, buffer_auxiliar	# Onde o caractere lido será salvo
+	li $a2, 1		# Número de caracteres a ser lido
+	syscall			
+	
+Concatena: # Concatena o caractere lido do arquivo texto no buffer de comparação
+	
+	lb $t2, 0($a1)			# Põe em $t2 o caractere atual do buffer auxiliar 
+	sb $t2, 0($t1)			# salva o caractere de $t2 no buffer de comparação
+	addi $t1, $t1, 1		# Seta o ponteiro axiliar $t1 para uma posição na memória a frente (para não sobre-escrever)
+	bne $v0, $zero, Pega_proximo	# Se a leitura do arquivo texto não for EOF leia mais um
+	
+	############### aqui deve ser um brench diferente. Coloquei esse pois era um jeito fácil para ver se o resto estava funcionando
+	############### deve-se comparar o que se tem no buffer de comparação com nossos valores do dicionário e fazer o fluxo do algoritmo
+	############### 
+	
+#Checa_dicionario: que tal abrir um outro arquivo para leitura de dicionário para realizar a checagem das chaves e valores?
+
+Escreve_Arquivo: # Escreve no arquivo dicionário o que estiver no buffer_de_comparação
+
+	li $v0, 15			# Código para escrita em arquivo
+	move $a0, $s1			# Descritor do arquivo a ser escrito
+	la $a1, buffer_de_comparação	# Dado a ser escrito 
+	li $a2, 42			# Esse valor tem q ser correspondente ao tamanho a ser escrito (42 era o exemplo que estava testando)
+	syscall
+	
+Fecha_Arquivos: # Fecha os arquivos usados no programa DEVES-SE mover para $a0 TODOS os descritores de arquivos usados no programa
+
+	li $v0, 16	# Código para fechar arquivo
+	move $a0, $s0	# Descritor do arquivo que será fechado (
+	syscall		
+	move $a0, $s1	# Descritor do arquivo que será fechado
+	syscall
+
+Fim: # Finaliza o programa
+	
+	li $v0, 16 # Código para finalizar o programa
+	syscall
