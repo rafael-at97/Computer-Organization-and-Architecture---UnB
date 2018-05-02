c38627823a1d4426b5196f88050a5050bbf9eaaf
rafael-at97 <rafael.a.t97@gmail.com>
2018-05-01 22:43:29 -0300

    Finished the compression and fixed the issue of last string already seen

--- compact2.0optmized.asm
+++ compact2.0optmized.asm
@@ -1,42 +1,53 @@
 .data
+	dictionary: .asciiz "dictionary.txt"	# Name of the file that contains the dictionary created during the compression
+						# Dictionary style: [index{string}]
 	textfile: .space 20 			## Name of the file to be compressed, the last char is reserved to '\0', so
 						## the name can be at most 19 chars long. Test if we can include the path
 						## in the name of the file, because it is only found in the same folder as the
 						## Mars.jar application 
-	dictionary: .asciiz "dictionary.txt"	# Name of the file that contains the dictionary created during the compression
-						# Dictionary style: [index(.word) -> string(.asciiz with the last one being '\0')]
-	# Both the buffers will be dynamic, especial care must be taken to avoid overwriting other values
-	buffer_aux: .asciiz ""			# Buffer that holds a single char read from the textfile
-	buffer_comp: .asciiz ""			## Buffer that concatenates chars and compares the resulting string with the ones
-						## in the dictionary
-	message: .asciiz "Nome do arquivo a ser lido"	# Show message,okay to be overwritten
-	bufferITOA: .space 8			# This Buffer is necessary to convert integer to ascii
+	bufferITOA: .asciiz "........-."	# This Buffer is necessary to convert integer to ascii
+	error_msg: .asciiz "Erro ao abrir arquivo!" # Error message to be displayed.
+	buffer: .asciiz "Nome do arquivo a ser compactado:" ## Buffer start with the message that will be displayed to the user,
+							    ## then it will concatenate chars and compare the resulting string with
+							    ## the ones in the dictionary					    
+
+.macro close_file %descriptor
+	li $v0, 16		# Close file syscall code
+	move $a0, %descriptor	# File descriptor
+	syscall 		
+.end_macro		
+							    							    					    							    
 .text
 	
-	## Details of what the variables are used for ##
-	# $s7 = used to store the size of the dictionary  	 			#
-	# $s0 = used to store the descriptor of the file to be read  			#
-	# $t0 = the counter of the buffer, we should change this to a saved register	#
-	# $t1 = the auxiliar counter of the pairs in the dictionary			#
-	# $t2 = Iterator for the strings in the dictionary				#
-	# $t3 = Auxiliar counter for the buffer						#
+	## 	Details of what the variables are used for in the 1st part	       ##
+	# $s0 = used to store the descriptor of the file to be compresseds		#
+	# $s1 = used to store the descriptor of the compressed file			#
+	# $s2 = The main counter of the buffer						#
+	# $s3 = Iterator for the strings in the dictionary				#
+	# $s4 = Auxiliar counter for the buffer						#
+	# $s5 = Saves the lenght of the string, used to jump to next pair		#
+	# $s6 = the auxiliar counter of the pairs in the dictionary			#
+	# $s7 = used to store the size of the dictionary  	 			#	
 	
+###### Part of the program that read the file name from the user
+		
 Get_Input: # Open the necessary files
 	
-	# Another method of reading the textfile name
+	# Java method of reading the textfile name
 	li $v0, 54
 	la $a1, textfile
 	li $a2, 20 	# n-1 chars will be read, the last one is reserved for '\0', remember to increase this size!!!
-	la $a0, message
+	la $a0, buffer 
 	syscall
 	
-	# Print a message in case the reading was not successful and exit, to do
-	bne $a1, $zero, End
+	# Print a message in case the reading was not successful and exit
+	bne $a1, $zero, Print_error
 	
-###### 	
+###### Just Filter the user input to remove the '\n'
+ 	
 	move $t0, $zero
 Filter_input:
-	# Transform the first '\n' in the input to a '\0', to avoid problems when opening the file with the same name
+	# Transform the first '\n' in the input to a '\0', to avoid problems when opening the file
 	lbu $t1, textfile($t0) 	 # Loads the current char into $t1
 	beq $t1, '\n', correct	 # Check if $t1 equals '\n'
 	beq $t1, $zero, continue # Check if $t1 equals '\0'
@@ -47,105 +58,220 @@ correct:
 	sb $zero, textfile($t0)
 	
 continue:
-######
 
-Open_File:
-	
+###### Open the file to be compressed and the compressed file where data will be written
+
+Open_Files:
 	# File to be compressed
 	li $v0, 13		# Open file code
 	la $a0, textfile	# Label	of the file
-	li $a1, 0		# flag 0
-	li $a2, 0		# Reading mode
+	li $a1, 0		# flag 0, reading
+	li $a2, 0		# Ignored mode
 	syscall			# Effectively open the file
 	move $s0, $v0		# Save the file descriptor in $s0
 	
 	# Possible error during opening file verification
-	beq $s0, -1, End
+	beq $s0, -1, Print_error
+
+	### Change the .txt to a .lzw ($t0 still holds the ending of the filename)
+	addi $t0, $t0, -1
+	addi $t1, $zero, 'w'
+	sb $t1, textfile($t0)
+	addi $t0, $t0, -1
+	addi $t1, $zero, 'z'
+	sb $t1, textfile($t0)
+	addi $t0, $t0, -1
+	addi $t1, $zero, 'l'
+	sb $t1, textfile($t0)		
+
+	# Compressed File opening ($a0 and $a2 already have the correct values)
+	li $v0, 13	# Open file syscall code
+	li $a1, 1	# Reading mode
+	syscall
+	move $s1, $v0
+
+	# Possible error during opening file verification
+	beq $s1, -1, Print_error
+
+###### Part where the program will continually read the file to be compressed and write the results in the compressed file
 
 	# The dictionary to store the pairs of keys and contents will be stored in the dynamic area, so, we will use $gp
-	# The dictionary start will be $gp and the counter will be $s7
-	move $s7, $zero
-	add $t0, $zero, $zero	# Works like a counter to the buffer
-Get_next: # Insert into buffer_comp the next char read
+	# The dictionary start will be $gp and the counter of how many pairs it have will be $s7
+	move $s7, $zero	# Size of the dictionary initially zero
+	move $s2, $zero	# Works like a counter to the buffer
+	move $v1, $zero	# Start the index to be written in the compressed file as zero
+		
+Get_next: # Insert into buffer the next char read
+	
 	li $v0, 14		 # Read file code
 	move $a0, $s0		 # File descriptor
-	la $a1, buffer_comp($t0) # Where the char will be saved
+	la $a1, buffer($s2) 	 # Where the char will be saved
 	li $a2, 1		 # Number of chars to be read
 	syscall
-	addi $t0, $t0, 1	 # Increment the index to get next position
 	
-	beq $v0, $zero, write_dictionary # If reached EOF, we must first save the string before leaving, to do
+	addi $s2, $s2, 1	 # Increment the index to get next position ready to receive a char
 	
-	# Compare the string starting in buffer_comp($zero) and ending into buffer_comp($t0) with the strings in the dictionary
-	move $t1, $zero		# Iterator for the dictionary
-	move $t2, $gp		# Declare iterator for the strings in the dictionary
-	move $t3, $zero		# Declare iterator for the string in the buffer	 
+	beq $v0, $zero, Check_last_string # If reached EOF, and $s2 is not zero, we must save the string that was last read
+	
+	# Compare the string starting in buffer_comp($zero) and ending into buffer_comp($s2) with the strings in the dictionary
+	move $s3, $gp		# Declare iterator for the memory of the dictionary
+	move $s4, $zero		# Declare iterator for the string in the buffer	 
+	move $s6, $zero		# Iterator for the pairs in the dictionary
 Compare:
-	beq $t1, $s7, add_dictionary	# No match found, insert into dictionary
-	# Save the length of the next string in the dictionary
-	lb $t6, 0($t2)
-	bne $t6, $t0, reset_and_next_prep # If the length of the buffer and the length of the string in the dictionary are different, go ahead to next string
-	addi $t2, $t2, 4  # If they are the same size, go comparing the string 
+	beq $s6, $s7, add_dictionary	  # No match found, insert into dictionary
+	lb $s5, 0($s3)			  # Get the length of the next string in the dictionary
+	bne $s5, $s2, reset_and_next_prep # If the length of the buffer and the length of the string in the dictionary are different, go ahead to next string
+	addi $s3, $s3, 4  	# If they are the same size, jump the .word that represents the size and go comparing the string
 	j keep_cmp
 
 add_dictionary:
-	# $t2 will already be in the position to store
-	addi $s7, $s7, 1 # The key for the new string to be inserted, start with a 1 because the 0 is assumed to be the empty string
-	sw $t0, 0($t2)   # Store the size of the next string
-	addi $t2, $t2, 4
-	move $t3, $zero # Iterator for the buffer string
+	## Every time we write something in the dictionary, it means there is a new string foud, so, we insert it into the compressed
+	#  file
+	jal write_compress
+	# $s3 will already be in the position to store
+	addi $s7, $s7, 1 # Increase the size of the dictionary in 1
+	sw $s2, 0($s3)   # Store the size of the next string
+	addi $s3, $s3, 4 # Jump the .word that indicates the size of the string
+	
+	move $t0, $zero  # Iterator for the buffer string
 repeat:	# Write the string into the dictionary
-	lb $t4, buffer_comp($t3)
-	sb $t4, 0($t2)
-	addi $t3, $t3, 1
-	addi $t2, $t2, 1
-	bne $t3, $t0, repeat
-	sb $zero, 0($t2) # End the string with a '\0'
-	move $t0, $zero # Reset buffer
+	lb $t1, buffer($t0) # Loads into $t0 the byte of the buffer
+	sb $t1, 0($s3)	    # Store the byte into the memory saved in $s3
+	addi $t0, $t0, 1
+	addi $s3, $s3, 1
+	bne $t0, $s2, repeat # $s2 indicates how many chars the buffer have
+	
+	move $s2, $zero      # Reset buffer size
 	j Get_next
 
 keep_cmp:
-	lb $t4, 0($t2)		   # Char from the dictionary
-	lb $t5, buffer_comp($t3)   # Char from the buffer
-	beq $t3, $t0, check_ending # Check for buffer end
+	lb $t0, 0($s3)		     # Char from the dictionary
+	lb $t1, buffer($s4)   	     # Char from the buffer
+	beq $s4, $s2, check_ending   # If $s2 and $4 are equal, the buffer ended, check if the dictionary's current string ended too
 	
-	bne $t4, $t5, reset_and_next # If different, do not continue
+	bne $t0, $t1, reset_and_next # If different, do not continue
 	# If they are equals, just read next byte
-	addi $t2, $t2, 1
-	addi $t3, $t3, 1
-	addi $t6, $t6, -1 	   # Decrease the size of the future jump to change indexes
+	addi $s3, $s3, 1
+	addi $s4, $s4, 1
+	addi $s5, $s5, -1 	     # Decrease the size of a possible future jump to change indexes
 	j keep_cmp
 	
-# Just go to the next position to insert the new pair
-reset_and_next_prep:
-	addi $t2, $t2, 4
+# Just go to the next position to compare with next pair
+reset_and_next_prep: # Jump the string size when sizes are different
+	addi $s3, $s3, 4
 reset_and_next:
-	move $t3, $zero   # Reset buffer counter
-	addi $t1, $t1, 1  # Increment 1 into the dictionary pair position
-	addi $t6, $t6, 1  # Add 1 more due to the '\0'
-	add $t2, $t2, $t6 # Jump to the end of the string
-	sub $t9, $t2, $gp # Get relative distance from position to start of dictionary
-	addi $t8, $zero, 4
-	div $t9, $t8
-	mfhi $t9	  # Get the remainder of the divison
-	beq $t9, $zero, Compare # If we are already at a valid position, do not add 4 more extra
-	sub $t9, $t8, $t9 # Bytes necessary to jump
-	add $t2, $t2, $t9 # Go to next index
+	move $s4, $zero   # Reset buffer counter
+	addi $s6, $s6, 1  # Increment 1 into the dictionary pair position
+	add $s3, $s3, $s5 # Jump to the address right after the end of the string
+	
+	### Routine necessary to go to the next pair
+	sub $t0, $s3, $gp 	# Get relative distance from position to start of dictionary (Avoid extra processing in the ULA?)
+	addi $t1, $zero, 4
+	div $t0, $t1
+	mfhi $t0	  	# Get the remainder of the divison by 4
+	beq $t0, $zero, Compare # If we are already at a valid position of a .word, do not add 4 more extra
+	sub $t0, $t1, $t0 	# Bytes necessary to jump to a valid .word address
+	add $s3, $s3, $t0 	# Go to next index
 	j Compare
 	
 check_ending:
-	# If the dictionary's current char is '\0', it is a match
-	beq $t4, $zero, match
+	# If the dictionary's current string size indicator $s5 is 0, it is a match
+	beq $s5, $zero, match
 	j reset_and_next
 match:
-	# If there is a match, simply go back to reading another char for the buffer and store the dictionary position
-	move $v1, $t1
+	# If there is a match, simply go back to reading another char for the buffer and store the dictionary index that caused the match
+	add $v1, $s6, 1	# The dictionary index is increased by 1 because the dictionary start with 1 but the loop ($s6) start with 0
 	j Get_next
 
-###
-# Write the dictionary file.txt from gp area of data
-write_dictionary:
+######
+
+write_compress:
+	### First of all, save the $ra in the $stack
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	# $v1 holds the index of the last string in the dictionary that matched with the buffer
+	# buffer($s2-1) indicates the char that broke that match
+	## First, we must turn the value $v1 into a string:
+	# Move the value into a parameter
+	move $a0, $v1
+	jal int_to_asciii
+	# After, $v0 will have the address of the strig to be printed and $v1 will have the number of chars to be written 
+	# We must save the char that broke the string into the last position of bufferITOA
+	la $t0, bufferITOA
+	addi $t1, $s2, -1 	# Index of the char to be saved
+	lb $t2, buffer($t1)	# Load the char into $t2
+	sb $t2, 9($t0)		# Writes the char into the correct position
 	
+	addi $a2, $v1, 2	# Increase the number of bytes to be saved by 2 and save in the parameter
+	move $a1, $v0		# Save the address to be printed
+	move $a0, $s1		# Descriptor of the compressed file
+	addi $v0, $zero, 15	# Write to file syscall code
+	syscall
+	
+	### Restore $v1 to 0 and also restore $ra
+	move $v1, $zero
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+######
+
+int_to_asciii:
+	# Function necessary because write file syscall only understands ascii
+	la $v0, bufferITOA
+	addi $v0, $v0, 7 # Save the address of the last char representing the index in the bufferITOA
+	move $t0, $a0	 # Move to $t0 which key will be converted to ascii	
+	move $v1, $zero	 # Number of chars that will be written, start with zero
+	beq $t0, $zero, back_zero # If the key is 0, just go to a special case to avoid the division
+	
+convert_int_asciii:
+	# Function that effectively convert integer into ascii
+	div $t0, $t0, 10 		# Divide by 10 so we can get the remainder and get a single digit at a time
+	mfhi $t1			# HI holds the remainder, saved now in $t1
+	addi $t1, $t1, 48		# Add 48 so we go to the ascii table position equivalent of the digit
+	sb $t1, 0($v0)			# Save the digit into the 'last' byte of $v0, that is the end of the buffer to be printed
+	addi $v0, $v0, -1		# Decrease $v0 to store now in another position
+	addi $v1, $v1, 1		# At the end of this loop $v1 will store how many chars it needs to print to compose the key
+	bne $t0, $zero, convert_int_asciii	# If $t0, that holds the current key, is zero, meaning that there are no more divisions to be made, exit the method
+	
+	addi $v0, $v0, 1 		# Return $v0 to the correct position, start of the bufferITOA that will be printed	
+	jr $ra
+back_zero:
+	addi $t0, $zero, 48	# 48 is the ascii code for '0'
+	sb $t0, 0($v0)		# Save this char into the correct position of the bufferITOA
+	addi $v1, $v1, 1	# Indicates that there is onde char to be written	
+	jr $ra			# Go back to callee
+			
+######
+
+# Check if we have to write the last string
+Check_last_string:
+	addi $t0, $zero, 1
+	beq $s2, $t0, write_dictionary
+	# If $s2 is not 1, we must prepare parameters and save the last string read
+	# $v1 is already ok, 
+	# We write a '\n' into the current position of $s2 and increase it by 1
+	addi $t0, $zero, 8 # Writes a 'backspace' to erase last char
+	sb $t0, buffer($s2)
+	addi $s2, $s2, 1
+	jal write_compress
+	
+	close_file $s0	# Close the file to be compressed
+	close_file $s1	# Close the already compressed file
+
+######
+
+	## 	Details of what the variables are used for in the 2nd part	       ##
+	# $s0 = used to store the descriptor of the dictionary				#
+	# $s1 = used to store the descriptor of the compressed file			#
+	# $s2 = The main counter of the buffer						#
+	# $s3 = Iterator for the strings in the dictionary				#
+	# $s4 = Auxiliar counter for the buffer						#
+	# $s5 = Saves the lenght of the string, used to jump to next pair		#
+	# $s6 = the auxiliar counter of the pairs in the dictionary			#
+	# $s7 = Still used to store the size of the dictionary 	 			#
+
+# Write the dictionary dictionary.txt from gp area of data
+write_dictionary:		
 	# $t2 = iterates in gp area of memory 							
 	# $t6 = how many keys have we printed so far, it needs to be equal $s7 to finish	
 	# $t1 = stores the address, and iterate in the buffer who will write in the archive	
@@ -161,18 +287,16 @@ write_dictionary:
 	move $s1, $v0		# Save the file descriptor in $s1
 	
 	# Possible error during opening file verification
-	beq $s1, -1, End
+	beq $s1, -1, Print_error
 	
 	# Initialize the variables
 	move $t2, $gp 		# Start in the begining of the gp area 
 	move $t6, $zero		# $t6 will count how many keys of the dictionary it have already printed
 
 start_printing:	
-	la $t1, buffer_comp 	# Buffer where the characters will be saved to print in the dictionary file	
-	move $t5, $zero 	# $t5 stores how many characters it will print in the file, this is necessary for syscall
+	la $t1, buffer	 	# Buffer where the characters will be saved to print in the dictionary file	
 		
-	lw $t0, 0($t2)		# The first value is the string length
-	add $t5, $t5, $t0
+	lw $t5, 0($t2)		# The first value is the string length, necessary for the syscall
 	add $t6, $t6, 1
 	jal int_to_ascii	# Convert integer key into equivalent ascii	
 	
@@ -199,13 +323,13 @@ next_byte:
 	add $t5, $t5, $t7 	# $t7 stores how many chars was stored in the buffer for each key
 
 	li $v0, 15			# Write on file code
-	la $a1, buffer_comp 		# From where it will be written
+	la $a1, buffer	 		# From where it will be written
 	move $a0, $s1			# File descriptor
 	move $a2, $t5			# Length of the string to be written
 	syscall
 	 
-	bne $s7, $t6, start_printing	# If there are more keyes in the dictionary do it again
-	j close_archives
+	bne $s7, $t6, start_printing	# If there are more keys in the dictionary do it again
+	j close_dictionary
 
 int_to_ascii:
 	# Function necessary because write file syscall only understands ascii
@@ -248,15 +372,19 @@ normalize:
 	add $t2, $t2, $t9 # Go to next index
 	jr $ra
 
-# Closes all archives used in the program
-close_archives:
-	li $v0, 16	# Close archive code
-	move $a0, $s0	# Original file descriptor 
-	syscall
-	move $a0, $s1	# Dictionay descriptor
+###### 
+
+# Closes the dictionary file
+close_dictionary:
+	close_file $s1
+	j End
+
+Print_error:	# Print a message in case could not open a file
+	li $v0, 55
+	la $a0, error_msg
+	add $a1, $zero, $zero
 	syscall
 
 End: # Kills the program
-	
 	li $v0, 10 # Syscall code to end program
 	syscall
